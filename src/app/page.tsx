ae266212bc234408ef706cf105581a379f904f60
jevinjojo <jevinjojo1@gmail.com>
2025-07-01 21:55:38 +0530

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client"
+import HomeContent from "./components/HomeComponent"
 
-import dynamic from "next/dynamic"
+// import dynamic from "next/dynamic"
 
-const HomeContent = dynamic(() => import("./components/HomeComponent"), {
-    ssr: false,
-})
+// const HomeContent = dynamic(() => import("./components/HomeComponent"), {
+//     ssr: false,
+// })
 
 export default function Home() {
     return <HomeContent />
